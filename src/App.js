5e4c671d49588e0dcc19945a768f007d7932414a
Lom209 <Lom209@users.noreply.github.com>
2020-11-20 16:51:08 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Alert from 'react-bootstrap/Alert';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
 
 function App() {
   return (
     <div>
     <Alert variant="success">
-<Alert.Heading>Hey, nice to see you</Alert.Heading>
+<Alert.Heading>Welcome to Fight Finder</Alert.Heading>
 <p>
-Aww yeah, you successfully read this important alert message. This example
-text is going to run a bit longer so that you can see how spacing within an
-alert works with this kind of content.
-</p>
-<hr />
-<p className="mb-0">
-Whenever you need to, be sure to use margin utilities to keep things nice
-and tidy.
+Come here to find information about events & tweets
 </p>
 </Alert>
-<a class="twitter-timeline" href="https://twitter.com/ufc?ref_src=twsrc%5Etfw">Tweets by ufc</a>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<div class="ufc-feed">
+<TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="ufc"
+  options={{height: 600, width: 250}}
+/>
+</div>
+<div class="bellator-feed">
+<TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="BellatorMMA"
+  options={{height: 600, width: 250}}
+/>
+</div>
+<div class="onefc-feed">
+<TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="ONEChampionship"
+  options={{height: 600, width: 250}}
+/>
+</div>
+<div class="cw-feed">
+<TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="CageWarriors"
+  options={{height: 600, width: 250}}
+/>
+</div>
+<div class="bkfc-feed">
+<TwitterTimelineEmbed
+  sourceType="profile"
+  screenName="bareknucklefc"
+  options={{height: 600, width: 250}}
+/>
+</div>
+
     </div>
   );
 }
