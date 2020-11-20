b58aa51b8bde14ba891e668ada84b5b0788fb699
Lom209 <Lom209@users.noreply.github.com>
2020-11-20 14:46:14 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <Alert variant="success">
+<Alert.Heading>Hey, nice to see you</Alert.Heading>
+<p>
+Aww yeah, you successfully read this important alert message. This example
+text is going to run a bit longer so that you can see how spacing within an
+alert works with this kind of content.
+</p>
+<hr />
+<p className="mb-0">
+Whenever you need to, be sure to use margin utilities to keep things nice
+and tidy.
+</p>
+</Alert>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Pokermeows - Coming Soon.
         </p>
-        <Alert variant="success">
-  <Alert.Heading>Hey, nice to see you</Alert.Heading>
-  <p>
-    Aww yeah, you successfully read this important alert message. This example
-    text is going to run a bit longer so that you can see how spacing within an
-    alert works with this kind of content.
-  </p>
-  <hr />
-  <p className="mb-0">
-    Whenever you need to, be sure to use margin utilities to keep things nice
-    and tidy.
-  </p>
-</Alert>
       </header>
     </div>
   );
