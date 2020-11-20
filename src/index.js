df1542a4cf3c634d263095200e774f5c2bd43453
Lom209 <Lom209@users.noreply.github.com>
2020-11-20 14:34:04 +0000

    updating bootsrap

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Alerts from 'react-bootstrap/Alerts';
+import Alert from 'react-bootstrap/Alert';
 
 ReactDOM.render(
   <React.StrictMode>
